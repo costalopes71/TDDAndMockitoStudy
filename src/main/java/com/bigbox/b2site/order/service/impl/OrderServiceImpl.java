a05b59b88eeca74a7873466a0f91aef033384220
Joao Lopes <joaolopes71@hotmail.com>
2019-05-20 22:33:26 -0300

    advanced features of mockito

--- src/main/java/com/bigbox/b2site/order/service/impl/OrderServiceImpl.java
+++ src/main/java/com/bigbox/b2site/order/service/impl/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package com.bigbox.b2site.order.service.impl;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.UUID;
 
 import com.bigbox.b2csite.order.model.domain.OrderSummary;
 import com.bigbox.b2csite.order.model.entity.OrderEntity;
@@ -13,6 +14,7 @@ import com.bigbox.b2site.order.service.OrderService;
 
 public class OrderServiceImpl implements OrderService {
 
+	private static final int MAX_INSERT_ATTEMP = 2;
 	private OrderDao orderDao = null;
 	private OrderEntityToOrderSummaryTransformer transformer = null;
 	
@@ -44,4 +46,38 @@ public class OrderServiceImpl implements OrderService {
 		return resultList;
 	}
 
+	public String openNewOrder(long customerId) throws ServiceException {
+		
+		OrderEntity newOrderEntity = new OrderEntity();
+		newOrderEntity.setCustomerId(customerId);
+		newOrderEntity.setOrderNumber(UUID.randomUUID().toString());
+		
+		boolean insertSuccessful = false;
+		int insertAttempt = 1;
+		while (!insertSuccessful && insertAttempt <= MAX_INSERT_ATTEMP) {
+			
+			try {
+				
+				int resultValue = orderDao.insert(newOrderEntity);
+				if (resultValue == 1) {
+					insertSuccessful = true;
+				} else {
+					++insertAttempt;
+				}
+				
+			} catch (DataAccessException e) {
+
+				++insertAttempt;
+			
+			}
+			
+		}
+		
+		if (!insertSuccessful) {
+			throw new ServiceException("Data access error prevented creation of order");
+		}
+		
+		return newOrderEntity.getOrderNumber();
+	}
+	
 }
--- src/test/java/com/bigbox/b2csite/order/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/bigbox/b2csite/order/service/impl/OrderServiceImplTest.java
@@ -1,33 +1,44 @@
 package com.bigbox.b2csite.order.service.impl;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.*;
 
 import java.util.LinkedList;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import com.bigbox.b2csite.order.model.domain.OrderSummary;
 import com.bigbox.b2csite.order.model.entity.OrderEntity;
 import com.bigbox.b2csite.order.model.transformer.OrderEntityToOrderSummaryTransformer;
+import com.bigbox.b2site.common.DataAccessException;
+import com.bigbox.b2site.common.ServiceException;
 import com.bigbox.b2site.order.dao.OrderDao;
 import com.bigbox.b2site.order.service.impl.OrderServiceImpl;
 
 public class OrderServiceImplTest {
 
 	private static final long CUSTOMER_ID = 1L;
-
+	
+	private OrderServiceImpl target = new OrderServiceImpl(); 
+	
+	@Mock
+	OrderDao mockOrderDao;
+	
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+		target.setOrderDao(mockOrderDao);
+	}
+	
 	@Test
 	public void testGetOrderSummarySuccess() throws Exception {
 		
 		// Setup
-		OrderServiceImpl target = new OrderServiceImpl();
-		
-		OrderDao mockOrderDao = Mockito.mock(OrderDao.class);
-		target.setOrderDao(mockOrderDao);
 		
 		OrderEntityToOrderSummaryTransformer mockTransformer = 
 				Mockito.mock(OrderEntityToOrderSummaryTransformer.class);
@@ -58,5 +69,71 @@ public class OrderServiceImplTest {
 		
 	}
 	
+	@Test
+	public void testOpenNewOrderSuccessfullyRetriesDataInsert() throws Exception {
+		
+		// setup
+		
+		Mockito.when(mockOrderDao.insert(Mockito.any(OrderEntity.class)))
+			.thenThrow(new DataAccessException("first exception"))
+			.thenReturn(1);
+		
+		//execution
+		
+		target.openNewOrder(CUSTOMER_ID);
+		
+		// verification
+		
+		Mockito.verify(mockOrderDao, Mockito.times(2)).insert(Mockito.any(OrderEntity.class));
+		
+	}
+	
+	@Test(expected=ServiceException.class)
+	public void testOpenNewOrderFailedDataInsert() throws Exception {
+		
+		// setup
+		Mockito.when(mockOrderDao.insert(Mockito.any(OrderEntity.class)))
+			.thenThrow(new DataAccessException("first exception"))
+			.thenThrow(new DataAccessException("second excepetion"));
+		
+		// execution
+		
+		try {
+			
+			target.openNewOrder(CUSTOMER_ID);
+			
+		} finally {
+			
+			Mockito.verify(mockOrderDao, Mockito.times(2)).insert(Mockito.any(OrderEntity.class));
+			
+		}
+		
+	}
+	
+	@Test
+	public void testOpenNewOrderSuccess() throws Exception {
+		
+		// setup
+		
+		Mockito.when(mockOrderDao.insert(Mockito.any()))
+			.thenReturn(1);
+
+		// execution
+		
+		String orderNumber = target.openNewOrder(CUSTOMER_ID);
+		
+		// verification
+		
+		ArgumentCaptor<OrderEntity> orderEntityCaptor = ArgumentCaptor.forClass(OrderEntity.class);
+		
+		Mockito.verify(mockOrderDao).insert(orderEntityCaptor.capture());
+		
+		OrderEntity capturedOrderEntity = orderEntityCaptor.getValue();
+		
+		assertNotNull(capturedOrderEntity);
+		assertEquals(CUSTOMER_ID, capturedOrderEntity.getCustomerId());
+		assertEquals(orderNumber, capturedOrderEntity.getOrderNumber());
+		
+	}
 	
 }
