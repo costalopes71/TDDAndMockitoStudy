3662f2d4e943e5b6601dbcfc44e695c86e37a9ff
Joao Lopes <joaolopes71@hotmail.com>
2019-05-23 22:46:19 -0300

    power mockito stubing static, final and private methods

--- src/test/java/com/bigbox/b2csite/order/service/impl/OrderServiceImplTestWithPowerMock.java
+++ src/test/java/com/bigbox/b2csite/order/service/impl/OrderServiceImplTestWithPowerMock.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNotNull;
 import java.util.LinkedList;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
@@ -20,13 +19,14 @@ import org.powermock.modules.junit4.PowerMockRunner;
 
 import com.bigbox.b2csite.order.dao.OrderDao;
 import com.bigbox.b2csite.order.integration.WarehouseManagementService;
+import com.bigbox.b2csite.order.model.domain.OrderCompletionAudit;
 import com.bigbox.b2csite.order.model.entity.OrderEntity;
 import com.bigbox.b2csite.order.model.entity.OrderItemEntity;
 import com.bigbox.b2csite.order.model.message.OrderMessage;
 import com.bigbox.b2csite.order.model.transformer.OrderEntityToOrderSummaryTransformer;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(value= {WarehouseManagementService.class})
+@PrepareForTest(value= {WarehouseManagementService.class, OrderServiceImpl.class})
 public class OrderServiceImplTestWithPowerMock {
 
 	private final static long CUSTOMER_ID = 1;
@@ -52,7 +52,10 @@ public class OrderServiceImplTestWithPowerMock {
 	@Test
 	public void testCompleteOrderSuccess() throws Exception {
 		
-		// preparacao
+		//
+		// setup
+		//
+		
 		OrderItemEntity itemFixture1 = new OrderItemEntity();
 		itemFixture1.setSku("SKU1");
 		itemFixture1.setQuantity(1);
@@ -74,10 +77,21 @@ public class OrderServiceImplTestWithPowerMock {
 		
 		PowerMockito.when(WarehouseManagementService.sendOrder(Matchers.any(OrderMessage.class))).thenReturn(true);
 		
+		// mocking object intantiations
+		OrderCompletionAudit orderAuditFixture = new OrderCompletionAudit();
+		PowerMockito.whenNew(OrderCompletionAudit.class).withAnyArguments().thenReturn(orderAuditFixture);
+		// ou
+		// PowerMockito.whenNew(OrderCompletionAudit.class).withNoArguments().thenReturn(orderAuditFixture);
+		
+		//
 		// execucao
+		//
 		target.completeOrder(ORDER_ID);
 		
+		//
 		// verificacao
+		//
+		
 		Mockito.verify(mockOrderDao).findById(ORDER_ID);
 		
 		PowerMockito.verifyStatic();
@@ -89,6 +103,8 @@ public class OrderServiceImplTestWithPowerMock {
 		
 		assertNotNull(capturedOrderMessage);
 		assertEquals(ORDER_NUMBER, capturedOrderMessage.getOrderNumber());
+		assertEquals(ORDER_NUMBER, orderAuditFixture.getOrderNumber());
+		assertNotNull(orderAuditFixture.getCompletionDate());
 		
 	}
 	
